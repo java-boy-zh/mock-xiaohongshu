5c1cb0b61c799f554dc93abe36475c2ed978ca12
王青玄 <1121586359@qq.com>
2025-05-25 10:31:11 +0800

    feat: 认证服务搭建

---
+++ common-auth/src/test/java/io/github/zh/commonauth/CommonAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zh.commonauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
