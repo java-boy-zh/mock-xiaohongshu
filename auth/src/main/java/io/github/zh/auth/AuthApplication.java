f000b5ae05ad701606be880b6bf2237c994c3321
王青玄 <1121586359@qq.com>
2025-05-27 16:13:14 +0800

    auth:  - 测试mybatis
    Signed-off-by: wb_wangzhe <wb_wangzhe@zhongan.com>

--- auth/src/main/java/io/github/zh/auth/AuthApplication.java
+++ auth/src/main/java/io/github/zh/auth/AuthApplication.java
@@ -1,9 +1,11 @@
 package io.github.zh.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"io.github.zh.auth.mapper"})
 public class AuthApplication {
 
     public static void main(String[] args) {
---
+++ auth/src/test/java/io/github/zh/auth/AuthApplicationTest.java
@@ -0,0 +1,45 @@
+package io.github.zh.auth;
+
+import io.github.zh.auth.domain.dataobject.UserDO;
+import io.github.zh.auth.mapper.UserDOMapper;
+import io.github.zh.common.util.JsonUtils;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Slf4j
+class AuthApplicationTest {
+
+    @Resource
+    private UserDOMapper userDOMapper;
+
+    /**
+     * 测试插入数据
+     */
+    @Test
+    void testInsert() {
+        UserDO userDO = UserDO.builder()
+                .username("王青玄")
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        userDOMapper.insert(userDO);
+    }
+
+    /**
+     * 查询数据
+     */
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 4 的记录
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
+    }
+
+}
