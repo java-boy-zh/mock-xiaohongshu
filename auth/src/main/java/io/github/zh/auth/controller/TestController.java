a9c7043cd2ebad4d942d43ca05a8b32b0722081c
王青玄 <1121586359@qq.com>
2025-05-27 13:51:37 +0800

    framework:  - 日志组件 优化 日期格式显示
    Signed-off-by: wb_wangzhe <wb_wangzhe@zhongan.com>

--- auth/src/main/java/io/github/zh/auth/controller/TestController.java
+++ auth/src/main/java/io/github/zh/auth/controller/TestController.java
@@ -1,6 +1,7 @@
 package io.github.zh.auth.controller;
 
 import io.github.zh.common.resopnse.Response;
+import io.github.zh.log.aspect.ApiOperationLog;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,12 @@ public class TestController {
 
     @GetMapping("/test")
     public Response<String> test() {
+        return Response.success("Hello, 这是测试返回");
+    }
+
+    @GetMapping("/test2")
+    @ApiOperationLog
+    public Response<String> test2() {
         return Response.success("Hello, 这是测试返回");
     }
 }
---
+++ framework/framework-common/src/main/java/io/github/zh/common/constants/DateConstants.java
@@ -0,0 +1,15 @@
+package io.github.zh.common.constants;
+
+/**
+ * DateConstants
+ *
+ * @author 王青玄
+ * @version v0.1 2025-05-27 13:49
+ */
+public interface DateConstants {
+
+    /**
+     * 年-月-日 时：分：秒
+     */
+    String Y_M_D_H_M_S_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}
