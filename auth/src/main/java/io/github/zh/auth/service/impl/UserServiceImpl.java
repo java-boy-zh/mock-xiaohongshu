0cd523705cdd991b94f77f2485647606692a7f34
王青玄 <1121586359@qq.com>
2025-05-31 18:52:34 +0800

    feat: 使用编程式事务控制用户注册

--- auth/src/main/java/io/github/zh/auth/service/impl/UserServiceImpl.java
+++ auth/src/main/java/io/github/zh/auth/service/impl/UserServiceImpl.java
@@ -18,13 +18,12 @@ import io.github.zh.common.eumns.StatusEnum;
 import io.github.zh.common.exception.BizException;
 import io.github.zh.common.resopnse.Response;
 import io.github.zh.common.util.JsonUtils;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -44,6 +43,7 @@ public class UserServiceImpl implements UserService {
     private final UserDOMapper userDOMapper;
     private final RedisTemplate<String, Object> redisTemplate;
     private final UserRoleRelDOMapper userRoleRelDOMapper;
+    private final TransactionTemplate transactionTemplate;
 
     /**
      * 登录与注册
@@ -115,47 +115,57 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 系统自动注册用户
+     *
      * @param phone
      * @return
      */
-    @Transactional(rollbackFor = Exception.class)
     public Long registerUser(String phone) {
-        // 获取全局自增的小红书 ID
-        Long xiaohashuId = redisTemplate.opsForValue().increment(RedisKeyConstants.XIAOHONGSHU_ID_GENERATOR_KEY);
 
-        UserDO userDO = UserDO.builder()
-                .phone(phone)
-                .xiaohongshuId(String.valueOf(xiaohashuId)) // 自动生成小红书号 ID
-                .nickname("小红薯" + xiaohashuId) // 自动生成昵称, 如：小红薯10000
-                .status(StatusEnum.ENABLE.getValue().byteValue()) // 状态为启用
-                .createTime(LocalDateTime.now())
-                .updateTime(LocalDateTime.now())
-                .isDeleted(DeletedEnum.NO.getValue()) // 逻辑删除
-                .build();
+        /*编程式事务控制回滚*/
+        return transactionTemplate.execute(status -> {
+            try {
+                // 获取全局自增的小红书 ID
+                Long xiaohashuId = redisTemplate.opsForValue().increment(RedisKeyConstants.XIAOHONGSHU_ID_GENERATOR_KEY);
 
-        // 添加入库
-        userDOMapper.insert(userDO);
+                UserDO userDO = UserDO.builder()
+                        .phone(phone)
+                        .xiaohongshuId(String.valueOf(xiaohashuId)) // 自动生成小红书号 ID
+                        .nickname("小红薯" + xiaohashuId) // 自动生成昵称, 如：小红薯10000
+                        .status(StatusEnum.ENABLE.getValue().byteValue()) // 状态为启用
+                        .createTime(LocalDateTime.now())
+                        .updateTime(LocalDateTime.now())
+                        .isDeleted(DeletedEnum.NO.getValue()) // 逻辑删除
+                        .build();
 
-        // 获取刚刚添加入库的用户 ID
-        Long userId = userDO.getId();
+                // 添加入库
+                userDOMapper.insert(userDO);
 
-        // 给该用户分配一个默认角色
-        UserRoleRelDO userRoleDO = UserRoleRelDO.builder()
-                .userId(userId)
-                .roleId(RoleConstants.COMMON_USER_ROLE_ID)
-                .createTime(LocalDateTime.now())
-                .updateTime(LocalDateTime.now())
-                .isDeleted(DeletedEnum.NO.getValue())
-                .build();
-        userRoleRelDOMapper.insert(userRoleDO);
+                // 获取刚刚添加入库的用户 ID
+                Long userId = userDO.getId();
 
-        // 将该用户的角色 ID 存入 Redis 中
-        List<Long> roles = Lists.newArrayList();
-        roles.add(RoleConstants.COMMON_USER_ROLE_ID);
-        String userRolesKey = RedisKeyConstants.buildUserRoleKey(phone);
-        redisTemplate.opsForValue().set(userRolesKey, JsonUtils.toJsonString(roles));
+                // 给该用户分配一个默认角色
+                UserRoleRelDO userRoleDO = UserRoleRelDO.builder()
+                        .userId(userId)
+                        .roleId(RoleConstants.COMMON_USER_ROLE_ID)
+                        .createTime(LocalDateTime.now())
+                        .updateTime(LocalDateTime.now())
+                        .isDeleted(DeletedEnum.NO.getValue())
+                        .build();
+                userRoleRelDOMapper.insert(userRoleDO);
 
-        return userId;
+                // 将该用户的角色 ID 存入 Redis 中
+                List<Long> roles = Lists.newArrayList();
+                roles.add(RoleConstants.COMMON_USER_ROLE_ID);
+                String userRolesKey = RedisKeyConstants.buildUserRoleKey(phone);
+                redisTemplate.opsForValue().set(userRolesKey, JsonUtils.toJsonString(roles));
+
+                return userId;
+            }catch (Exception e){
+                status.setRollbackOnly(); // 标记事务为回滚
+                log.error("==> 系统注册用户异常: ", e);
+                return null;
+            }
+        });
     }
 
 }
