734088077054fbe5c71e3ab7d579189de0bbe0d5
王青玄 <1121586359@qq.com>
2025-05-31 21:29:37 +0800

    feat: Gateway网关搭建

---
+++ gateway/src/main/java/io/github/zh/gateway/XiaohongshuGatewayApplication.java
@@ -0,0 +1,16 @@
+package io.github.zh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王青玄
+ * @Contact 1121586359@qq.com
+ * @create 2025年05月31日 21:28
+ */
+@SpringBootApplication
+public class XiaohongshuGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuGatewayApplication.class, args);
+    }
+}
