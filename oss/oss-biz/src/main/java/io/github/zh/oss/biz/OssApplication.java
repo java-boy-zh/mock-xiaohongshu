eedf93fbd9f73bb7874d661bf20998571ea013d1
王青玄 <1121586359@qq.com>
2025-06-01 17:37:48 +0800

    feat: OSS模块开发

---
+++ oss/oss-biz/src/main/java/io/github/zh/oss/biz/OssApplication.java
@@ -0,0 +1,18 @@
+package io.github.zh.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王青玄
+ * @Contact 1121586359@qq.com
+ * @create 2025年06月01日 17:29
+ */
+@SpringBootApplication
+public class OssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+
+}
