ae6cd6698458e61f6cb3c4b2faf79db88fce6361
王青玄 <1121586359@qq.com>
2025-06-01 20:07:12 +0800

    feat: 用户中心服务搭建

---
+++ usercenter/usercenter-server/src/main/java/io/github/zh/usercenter/server/UserCenterServerApplication.java
@@ -0,0 +1,17 @@
+package io.github.zh.usercenter.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王青玄
+ * @Contact 1121586359@qq.com
+ * @create 2025年06月01日 20:04
+ */
+@SpringBootApplication
+public class UserCenterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterServerApplication.class, args);
+    }
+}
