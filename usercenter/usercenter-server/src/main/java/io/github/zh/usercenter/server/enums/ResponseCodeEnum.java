45e7e2d4afaab4cd295b0ff939e0eaa313bbf50c
王青玄 <1121586359@qq.com>
2025-06-01 20:35:41 +0800

    feat: 用户中心 全局异常处理

---
+++ usercenter/usercenter-server/src/main/java/io/github/zh/usercenter/server/enums/ResponseCodeEnum.java
@@ -0,0 +1,29 @@
+package io.github.zh.usercenter.server.enums;
+
+import io.github.zh.common.exception.BaseExceptionInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * ResponseCodeEnum
+ *
+ * @author 王青玄
+ * @version v0.1 2025-05-28 13:18
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseCodeEnum implements BaseExceptionInterface {
+
+    // ----------- 通用异常状态码 -----------
+    SYSTEM_ERROR("USERCENTER-10000", "出错啦，后台小哥正在努力修复中..."),
+    PARAM_NOT_VALID("USERCENTER-10001", "参数错误"),
+
+    // ----------- 业务异常状态码 -----------
+    ;
+
+    // 异常码
+    private final String errorCode;
+    // 错误信息
+    private final String errorMessage;
+
+}
