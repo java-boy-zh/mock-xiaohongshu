6027e5228d1112410e7b5b3436b3d89f746b12ff
王青玄 <1121586359@qq.com>
2025-06-01 14:04:24 +0800

    feat: 全局组件封装

---
+++ framework/framework-starter-context/src/main/java/io/github/zh/context/config/ContextAutoConfiguration.java
@@ -0,0 +1,22 @@
+package io.github.zh.context.config;
+
+import io.github.zh.context.filter.HeaderUserId2ContextFilter;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 王青玄
+ * @Contact 1121586359@qq.com
+ * @create 2025年06月01日 13:55
+ */
+@AutoConfiguration
+public class ContextAutoConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<HeaderUserId2ContextFilter> filterFilterRegistrationBean() {
+        HeaderUserId2ContextFilter filter = new HeaderUserId2ContextFilter();
+        FilterRegistrationBean<HeaderUserId2ContextFilter> bean = new FilterRegistrationBean<>(filter);
+        return bean;
+    }
+}
