15300bee87cfe5223a34a8bef2ffd233344c8f9a
王青玄 <1121586359@qq.com>
2025-06-11 13:18:38 +0800

    auth:   - 用户注册重构
    Signed-off-by: wb_wangzhe <wb_wangzhe@zhongan.com>

--- auth/src/main/java/io/github/zh/auth/AuthApplication.java
+++ auth/src/main/java/io/github/zh/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package io.github.zh.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan({"io.github.zh.auth.mapper"})
