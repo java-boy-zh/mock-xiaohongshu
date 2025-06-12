a8cb41a1342680bae615116c8863bf8a10c98d65
王青玄 <1121586359@qq.com>
2025-06-12 16:28:54 +0800

    kv:   - 短文本存储服务搭建并注册到Nacos
    Signed-off-by: wb_wangzhe <wb_wangzhe@zhongan.com>

---
+++ xiaohongshu-kv/xiaohongshu-kv-server/src/main/java/io/github/zh/kv/server/KVServerApplication.java
@@ -0,0 +1,17 @@
+package io.github.zh.kv.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * KVServerApplication
+ *
+ * @author 王青玄
+ * @version v0.1 2025-06-12 16:23
+ */
+@SpringBootApplication
+public class KVServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KVServerApplication.class, args);
+    }
+}
