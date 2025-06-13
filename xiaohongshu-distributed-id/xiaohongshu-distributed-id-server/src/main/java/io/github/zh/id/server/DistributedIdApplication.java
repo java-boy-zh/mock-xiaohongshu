5ff8cae70c6f0263a7970d412f3e41d6febc2e21
王青玄 <1121586359@qq.com>
2025-06-13 16:43:51 +0800

    id分发:   - 基础项目结构搭建
    Signed-off-by: wb_wangzhe <wb_wangzhe@zhongan.com>

---
+++ xiaohongshu-distributed-id/xiaohongshu-distributed-id-server/src/main/java/io/github/zh/id/server/DistributedIdApplication.java
@@ -0,0 +1,17 @@
+package io.github.zh.id.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DistributedIdApplication
+ *
+ * @author 王青玄
+ * @version v0.1 2025-06-13 16:41
+ */
+@SpringBootApplication
+public class DistributedIdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedIdApplication.class, args);
+    }
+}
