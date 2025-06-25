93004e45c7c8838695abf91c0dd9377271ea432e
王青玄 <1121586359@qq.com>
2025-06-25 17:54:14 +0800

    userrelation:   - 用户关系服务发送MQ的消息消费者构建
    Signed-off-by: zawb-wangzhe <wb-wangzhe@zhongan.io>

---
+++ xiaohongshu-user-relation/xiaohongshu-user-relation-server/src/main/java/io/github/zh/userrelation/server/consumer/FollowUnfollowConsumer.java
@@ -0,0 +1,107 @@
+package io.github.zh.userrelation.server.consumer;
+
+import io.github.zh.common.util.JsonUtils;
+import io.github.zh.userrelation.server.constant.MQConstants;
+import io.github.zh.userrelation.server.domain.dataobject.FansDO;
+import io.github.zh.userrelation.server.domain.dataobject.FollowingDO;
+import io.github.zh.userrelation.server.domain.dto.FollowUserMqDTO;
+import io.github.zh.userrelation.server.mapper.FansDOMapper;
+import io.github.zh.userrelation.server.mapper.FollowingDOMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * FollowUnfollowConsumer
+ *
+ * @author 王青玄
+ * @version v0.1 2025-06-25 17:48
+ * @description: 关注、取关 MQ 消费者
+ */
+@Component
+@RocketMQMessageListener(consumerGroup = "xiaohongshu_group", // Group 组
+        topic = MQConstants.TOPIC_FOLLOW_OR_UNFOLLOW // 消费的 Topic 主题
+)
+@Slf4j
+public class FollowUnfollowConsumer implements RocketMQListener<Message> {
+
+    @Resource
+    private FollowingDOMapper followingDOMapper;
+    @Resource
+    private FansDOMapper fansDOMapper;
+    @Resource
+    private TransactionTemplate transactionTemplate;
+
+    @Override
+    public void onMessage(Message message) {
+        // 消息体
+        String bodyJsonStr = new String(message.getBody());
+        // 标签
+        String tags = message.getTags();
+
+        log.info("==> FollowUnfollowConsumer 消费了消息 {}, tags: {}", bodyJsonStr, tags);
+
+        // 根据 MQ 标签，判断操作类型
+        if (Objects.equals(tags, MQConstants.TAG_FOLLOW)) { // 关注
+            handleFollowTagMessage(bodyJsonStr);
+        } else if (Objects.equals(tags, MQConstants.TAG_UNFOLLOW)) { // 取关
+            // TODO
+        }
+    }
+
+    /**
+     * 关注
+     * @param bodyJsonStr
+     */
+    private void handleFollowTagMessage(String bodyJsonStr) {
+        // 将消息体 Json 字符串转为 DTO 对象
+        FollowUserMqDTO followUserMqDTO = JsonUtils.parseObject(bodyJsonStr, FollowUserMqDTO.class);
+
+        // 判空
+        if (Objects.isNull(followUserMqDTO)) return;
+
+        // 幂等性：通过联合唯一索引保证
+
+        Long userId = followUserMqDTO.getUserId();
+        Long followUserId = followUserMqDTO.getFollowUserId();
+        LocalDateTime createTime = followUserMqDTO.getCreateTime();
+
+        // 编程式提交事务
+        boolean isSuccess = Boolean.TRUE.equals(transactionTemplate.execute(status -> {
+            try {
+                // 关注成功需往数据库添加两条记录
+                // 关注表：一条记录
+                int count = followingDOMapper.insert(FollowingDO.builder()
+                        .userId(userId)
+                        .followingUserId(followUserId)
+                        .createTime(createTime)
+                        .build());
+
+                // 粉丝表：一条记录
+                if (count > 0) {
+                    fansDOMapper.insert(FansDO.builder()
+                            .userId(followUserId)
+                            .fansUserId(userId)
+                            .createTime(createTime)
+                            .build());
+                }
+                return true;
+            } catch (Exception ex) {
+                status.setRollbackOnly(); // 标记事务为回滚
+                log.error("", ex);
+            }
+            return false;
+        }));
+
+        log.info("## 数据库添加记录结果：{}", isSuccess);
+        // TODO: 更新 Redis 中被关注用户的 ZSet 粉丝列表
+    }
+
+}
