c5d458c8eeeb62999bc16d3e44352a49e9a222f1
王青玄 <1121586359@qq.com>
2025-06-27 09:39:07 +0800

    count:   - 整合快手 Buffer Trigger
    Signed-off-by: zawb-wangzhe <wb-wangzhe@zhongan.io>

--- xiaohongshu-count/xiaohongshu-count-server/src/main/java/io/github/zh/count/server/consumer/CountFansConsumer.java
+++ xiaohongshu-count/xiaohongshu-count-server/src/main/java/io/github/zh/count/server/consumer/CountFansConsumer.java
@@ -1,10 +1,15 @@
 package io.github.zh.count.server.consumer;
 
+import com.github.phantomthief.collection.BufferTrigger;
+import io.github.zh.common.util.JsonUtils;
 import io.github.zh.count.server.constant.MQConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.List;
 
 /**
  * CountFansConsumer
@@ -18,13 +23,25 @@ import org.springframework.stereotype.Component;
         topic = MQConstants.TOPIC_COUNT_FANS // 主题 Topic
 )
 @Slf4j
-public class CountFansConsumer implements RocketMQListener<String> { // TODO: Message
+public class CountFansConsumer implements RocketMQListener<String> {
+
+    private BufferTrigger<String> bufferTrigger = BufferTrigger.<String>batchBlocking()
+            .bufferSize(50000) // 缓存队列的最大容量
+            .batchSize(1000)   // 一批次最多聚合 1000 条
+            .linger(Duration.ofSeconds(1)) // 多久聚合一次
+            .setConsumerEx(this::consumeMessage)
+            .build();
 
 
     @Override
     public void onMessage(String body) {
-        log.info("## 消费到了 MQ 【计数: 粉丝数】, {}...", body);
+        // 往 bufferTrigger 中添加元素
+        bufferTrigger.enqueue(body);
     }
 
+    private void consumeMessage(List<String> bodys) {
+        log.info("==> 聚合消息, size: {}", bodys.size());
+        log.info("==> 聚合消息, {}", JsonUtils.toJsonString(bodys));
+    }
 
 }
