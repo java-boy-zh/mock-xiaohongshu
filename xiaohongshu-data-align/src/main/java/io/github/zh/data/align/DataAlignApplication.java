78a3afc094af0bce963be657bdf94460eead6dea
王青玄 <1121586359@qq.com>
2025-06-27 14:26:54 +0800

    dDataAlign:   - 数据对齐服务搭建
    Signed-off-by: zawb-wangzhe <wb-wangzhe@zhongan.io>

---
+++ xiaohongshu-data-align/src/main/java/io/github/zh/data/align/DataAlignApplication.java
@@ -0,0 +1,19 @@
+package io.github.zh.data.align;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DataAlignApplication
+ *
+ * @author 王青玄
+ * @version v0.1 2025-06-27 14:23
+ */
+@SpringBootApplication
+@MapperScan("io.github.zh.data.align.domain.mapper")
+public class DataAlignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataAlignApplication.class, args);
+    }
+}
