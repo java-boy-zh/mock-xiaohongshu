244ca019c8bd87200a0a53f55fd37e89d7eded1a
王青玄 <1121586359@qq.com>
2025-06-27 16:20:04 +0800

    Search:   - 基于Es的搜索服务搭建
    Signed-off-by: zawb-wangzhe <wb-wangzhe@zhongan.io>

---
+++ xiaohongshu-search/src/main/java/io/github/zh/search/SearchApplication.java
@@ -0,0 +1,16 @@
+package io.github.zh.search;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * SearchApplication
+ *
+ * @author 王青玄
+ * @version v0.1 2025-06-27 16:15
+ */
+@SpringBootApplication
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
