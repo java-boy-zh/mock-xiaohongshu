4cf7c5923ddd4731930140f67fa6437d45278b51
王青玄 <1121586359@qq.com>
2025-06-27 10:02:51 +0800

    count:   - 关注数据入库
    Signed-off-by: zawb-wangzhe <wb-wangzhe@zhongan.io>

--- xiaohongshu-count/xiaohongshu-count-server/src/main/java/io/github/zh/count/server/mapper/UserCountDOMapper.java
+++ xiaohongshu-count/xiaohongshu-count-server/src/main/java/io/github/zh/count/server/mapper/UserCountDOMapper.java
@@ -25,4 +25,14 @@ public interface UserCountDOMapper {
     int insertOrUpdateFansTotalByUserId(@Param("count") Integer count,
                                         @Param("userId") Long userId);
 
+    /**
+     * 添加或更新关注总数
+     * @param count
+     * @param userId
+     * @return
+     */
+    int insertOrUpdateFollowingTotalByUserId(@Param("count") Integer count,
+                                             @Param("userId") Long userId);
+
+
 }
